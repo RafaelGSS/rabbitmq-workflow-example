c7e3a6bd96aad648f6202a208cbc285660662975
RafaelGSS <rafael.nunu@hotmail.com>
2019-06-30 23:30:27 -0300

    fix: Adjust rpc producer and consume

--- rpc/consumer.js
+++ rpc/consumer.js
@@ -1,22 +1,25 @@
-const { createConnection } = require('../../connection')
+const { createConnection } = require('../connection')
 const { sample } = require('lodash')
 const uuid = require('uuid/v4')
 
 createConnection()
   .then(conn => conn.createChannel())
-  .then(ch => {
+
+  .then(async ch => {
     console.log('Channel created!')
 
-    const q = ch.assertQueue('', { exclusive: true })
-    
+    const q = await ch.assertQueue('', { exclusive: true })
+    const queue = 'rpc_queue'
+
     const correlationId = uuid()
-    console.log(' [x] Sending message - correlation: %s', correlationId)
-    ch.sendToQueue('rpc_queue', Buffer.from(sample['10', '11', '1']), { 
+    console.log(' [x] Sending message - correlation: %s - replyTo: %s', correlationId, q.queue)
+
+    ch.sendToQueue(queue, Buffer.from(sample(['10', '11', '1'])), {
       replyTo: q.queue, correlationId
-    }) 
+    })
     ch.consume(q.queue, function (msg) {
-      if (msg !== null && msg.properties.correlationId =- correlationId) {
-        console.log('[%s] Received: %s', correlationId, msg.content.toString())
+      if (msg !== null && msg.properties.correlationId === correlationId) {
+        console.log('[%s] Received: %s', msg.properties.correlationId, msg.content.toString())
       }
     }, { noAck: true })
   })
